a58a494def5ad5ec0c93d8037b1bd95a7c0129d6
chakraborty <chakraborty@silexica.com>
2020-05-20 16:03:38 +0200

    conditional rendering

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -16,32 +16,41 @@ class Movies extends Component {
     }
 
     render() {
-        return (
-            <table className="table">
+        const { length: movieCount } = this.state.movies;
+        if (movieCount === 0) {
+            return <p>There are no movies in the database</p>
+        } else {
+            return (
+                <React.Fragment>
+                    <p>Showing {movieCount} movies in the database.</p>
+                    <table className="table">
 
-                <thead>
-                    <tr>
-                        <th scope="col">Title</th>
-                        <th scope="col">Genre</th>
-                        <th scope="col">Stock</th>
-                        <th scope="col">Rate</th>
-                        <th scope="col"></th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                    {
-                        this.state.movies.map(movie => (
-                            <tr key={movie._id} >
-                                <th scope="row" >{movie.title}</th>
-                                <td >{movie.genre.name}</td>
-                                <td> {movie.numberInStock}</td>
-                                <td> {movie.dailyRentalRate}</td>
-                                <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
+                        <thead>
+                            <tr>
+                                <th scope="col">Title</th>
+                                <th scope="col">Genre</th>
+                                <th scope="col">Stock</th>
+                                <th scope="col">Rate</th>
+                                <th scope="col"></th>
                             </tr>
-                        ))}
-                </tbody>
-            </table>);
+                        </thead>
+                        <tbody>
+
+                            {
+                                this.state.movies.map(movie => (
+                                    <tr key={movie._id} >
+                                        <th scope="row" >{movie.title}</th>
+                                        <td >{movie.genre.name}</td>
+                                        <td> {movie.numberInStock}</td>
+                                        <td> {movie.dailyRentalRate}</td>
+                                        <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
+                                    </tr>
+                                ))}
+                        </tbody>
+                    </table>
+                </React.Fragment>
+            );
+        }
     }
 }
 
