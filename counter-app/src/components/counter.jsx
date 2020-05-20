0e3750242f97bacd10c07a65370c80b7bdb54c61
chakraborty <chakraborty@silexica.com>
2020-05-20 14:12:49 +0200

    updating the state

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -5,12 +5,23 @@ class Counter extends Component {
         count: 0,
     }
 
+
+    handleIncrement = () => {
+        // console.log(this);
+
+        this.setState(
+            {
+                count: this.state.count + 1
+            }
+        );
+    }
+
     render() {
 
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
