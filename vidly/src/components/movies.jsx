03bf360d2a5776a3155ee547d550d7199c863cd6
chakraborty <chakraborty@silexica.com>
2020-05-20 15:45:18 +0200

    render rows and handle delete movie

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -7,33 +7,41 @@ class Movies extends Component {
 
     handleDelete = movie => {
 
+        console.log(movie)
+
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({
+            movies
+        })
     }
 
     render() {
-        return (<table class="table">
+        return (
+            <table className="table">
 
-            <thead>
-                <tr>
-                    <th scope="col">Title</th>
-                    <th scope="col">Genre</th>
-                    <th scope="col">Stock</th>
-                    <th scope="col">Rate</th>
-                    <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
+                <thead>
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col">Genre</th>
+                        <th scope="col">Stock</th>
+                        <th scope="col">Rate</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
 
-                {
-                    this.state.movies.map(movie => (
-                        <tr >
-                            <th scope="row" >{movie.title}</th>
-                            <td >{movie.genre.name}</td>
-                            <td> {movie.numberInStock}</td>
-                            <td> {movie.dailyRentalRate}</td>
-                        </tr>
-                    ))}
-            </tbody>
-        </table>);
+                    {
+                        this.state.movies.map(movie => (
+                            <tr key={movie._id} >
+                                <th scope="row" >{movie.title}</th>
+                                <td >{movie.genre.name}</td>
+                                <td> {movie.numberInStock}</td>
+                                <td> {movie.dailyRentalRate}</td>
+                                <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
+                            </tr>
+                        ))}
+                </tbody>
+            </table>);
     }
 }
 
