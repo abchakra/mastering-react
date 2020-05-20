6a7aeb51b6322be75cbb123e7138bf8235319fbc
chakraborty <chakraborty@silexica.com>
2020-05-20 15:39:38 +0200

    Add movies component

---
+++ vidly/src/components/movies.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { getMovies } from "../services/fakeMovieService"
+class Movies extends Component {
+    state = {
+        movies: getMovies()
+    }
+
+    handleDelete = movie => {
+
+    }
+
+    render() {
+        return (<table class="table">
+
+            <thead>
+                <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Genre</th>
+                    <th scope="col">Stock</th>
+                    <th scope="col">Rate</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+
+                {
+                    this.state.movies.map(movie => (
+                        <tr >
+                            <th scope="row" >{movie.title}</th>
+                            <td >{movie.genre.name}</td>
+                            <td> {movie.numberInStock}</td>
+                            <td> {movie.dailyRentalRate}</td>
+                        </tr>
+                    ))}
+            </tbody>
+        </table>);
+    }
+}
+
+export default Movies;
