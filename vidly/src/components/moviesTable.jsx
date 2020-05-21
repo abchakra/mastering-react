0369af37920251480745b0b78075ed378819b66c
chakraborty <chakraborty@silexica.com>
2020-05-21 16:12:53 +0200

    Extract Table component

--- vidly/src/components/moviesTable.jsx
+++ vidly/src/components/moviesTable.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import TableHeader from './common/tableHeader';
-import TableBody from './common/tableBody';
 import Like from './like';
+import Table from './common/table';
 class MoviesTable extends Component {
 
     columns = [
@@ -14,16 +13,9 @@ class MoviesTable extends Component {
 
     ]
     render() {
-        const { movies, onDelete, onLike, sortColumn, onSort } = this.props;
+        const { movies, sortColumn, onSort } = this.props;
         return (
-            <table className="table">
-
-                <TableHeader
-                    columns={this.columns}
-                    sortColumn={sortColumn}
-                    onSort={onSort} />
-                <TableBody data={movies} columns={this.columns} onLike={onLike} onDelete={onDelete} />
-            </table>
+            <Table columns={this.columns} onSort={onSort} data={movies} sortColumn={sortColumn} />
         );
     }
 }
