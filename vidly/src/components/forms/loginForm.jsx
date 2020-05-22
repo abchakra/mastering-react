7e03b36629dbe7935a0623fbe7a614354808579c
chakraborty <chakraborty@silexica.com>
2020-05-22 14:42:24 +0200

    validation on change

--- vidly/src/components/forms/loginForm.jsx
+++ vidly/src/components/forms/loginForm.jsx
@@ -23,6 +23,7 @@ class LoginForm extends Component {
     }
     return Object.keys(errors).length === 0 ? null : errors;
   };
+
   handleSubmit = (e) => {
     e.preventDefault();
 
@@ -33,11 +34,24 @@ class LoginForm extends Component {
     if (errors) return;
     console.log("Submitted");
   };
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required";
+    }
 
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required";
+    }
+  };
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
